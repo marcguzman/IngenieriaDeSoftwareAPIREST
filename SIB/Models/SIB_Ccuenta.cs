15d1d31877efee65444189ea940777be07d4e99d
marcguzman <marcguzman779@gmail.com>
2021-06-10 17:47:28 -0600

    ConsultaCuenta

---
+++ SIB/Models/SIB_Ccuenta.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace SIB.Models
+{
+    public class SIB_Ccuenta
+    {
+        public int Id { get; set; }
+        public int NoCuenta { get; set; }
+        public string Banco { get; set; }
+        public string TipoCuenta { get; set; }
+        public int Saldo { get; set; }
+    }
+}
